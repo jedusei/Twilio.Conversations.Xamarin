bcb27f13746fbba997e0c5a8cf1cf7eeb5544ddf
Joseph Edusei <jedusei99@gmail.com>
2021-06-03 06:51:52 +0000

    corrected listener invoker namespaces.

--- Twilio.Conversations.Droid/Additions/Fixes/ICallbackListener.cs
+++ Twilio.Conversations.Droid/Additions/Fixes/ICallbackListener.cs
@@ -5,7 +5,7 @@ namespace Twilio.Conversations
 {
     public partial interface ICallbackListener : IJavaObject, IJavaPeerable
     {
-        [Register("onError", "(Lcom/twilio/conversations/ErrorInfo;)V", "GetOnError_Lcom_twilio_conversations_ErrorInfo_Handler:Com.Twilio.Conversations.ICallbackListenerInvoker, Twilio.Conversations.Droid")]
+        [Register("onError", "(Lcom/twilio/conversations/ErrorInfo;)V", "GetOnError_Lcom_twilio_conversations_ErrorInfo_Handler:Twilio.Conversations.ICallbackListenerInvoker, Twilio.Conversations.Droid")]
         void OnError(ErrorInfo errorInfo)
         {
             // Had to add this manually because it was ignored by the bindings generator.
--- Twilio.Conversations.Droid/Additions/Fixes/IStatusListener.cs
+++ Twilio.Conversations.Droid/Additions/Fixes/IStatusListener.cs
@@ -5,7 +5,7 @@ namespace Twilio.Conversations
 {
     public partial interface IStatusListener : IJavaObject, IJavaPeerable
     {
-        [Register("onError", "(Lcom/twilio/conversations/ErrorInfo;)V", "GetOnError_Lcom_twilio_conversations_ErrorInfo_Handler:Com.Twilio.Conversations.IStatusListenerInvoker, Twilio.Conversations.Droid")]
+        [Register("onError", "(Lcom/twilio/conversations/ErrorInfo;)V", "GetOnError_Lcom_twilio_conversations_ErrorInfo_Handler:Twilio.Conversations.IStatusListenerInvoker, Twilio.Conversations.Droid")]
         void OnError(ErrorInfo errorInfo)
         {
             // Had to add this manually because it was ignored by the bindings generator.

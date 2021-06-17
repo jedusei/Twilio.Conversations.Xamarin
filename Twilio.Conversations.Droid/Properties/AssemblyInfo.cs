57ddf149be7a40791d4b6540abf5ed61ff465550
Joseph Edusei <jedusei99@gmail.com>
2021-06-17 18:03:12 +0000

    Release 1.0.2

--- Twilio.Conversations.Droid/Properties/AssemblyInfo.cs
+++ Twilio.Conversations.Droid/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
--- Twilio.Conversations.iOS/Properties/AssemblyInfo.cs
+++ Twilio.Conversations.iOS/Properties/AssemblyInfo.cs
@@ -1,12 +1,10 @@
 ﻿using System.Reflection;
 
-using Foundation;
-
 // This attribute allows you to mark your assemblies as “safe to link”.
 // When the attribute is present, the linker—if enabled—will process the assembly
 // even if you’re using the “Link SDK assemblies only” option, which is the default for device builds.
 
-[assembly: LinkerSafe]
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
@@ -24,7 +22,7 @@ using Foundation;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.2")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.

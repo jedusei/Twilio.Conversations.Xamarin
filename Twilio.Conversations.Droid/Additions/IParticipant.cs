06106d714e99f29f39fd4b56c0ea51d20da8fdfb
Joseph Edusei <jedusei99@gmail.com>
2022-06-18 09:26:25 +0000

    add IParticipant C# async-style methods.

---
+++ Twilio.Conversations.Droid/Additions/IParticipant.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace Twilio.Conversations
+{
+    public partial interface IParticipant
+    {
+        public async Task<IUser> GetAndSubscribeUserAsync()
+        {
+            AsyncCallbackListener<IUser> listener = new();
+            GetAndSubscribeUser(listener);
+            return await listener.GetResultAsync();
+        }
+
+        public async Task SetAttributesAsync(JsonAttributes attributes)
+        {
+            AsyncStatusListener listener = new();
+            SetAttributes(attributes, listener);
+            await listener.Task;
+        }
+
+        public async Task RemoveAsync()
+        {
+            AsyncStatusListener listener = new();
+            Remove(listener);
+            await listener.Task;
+        }
+    }
+}
